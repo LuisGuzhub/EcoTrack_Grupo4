2c9e9ae01ae94c443072c166907325394416cff6
Kevin Vera <keviherr@espol.edu.ec>
2025-11-09 21:41:47 -0500

    Update ComparadorPeso.java

--- src/ecotrack/ComparadorPeso.java
+++ src/ecotrack/ComparadorPeso.java
@@ -9,16 +9,3 @@ public class ComparadorPeso implements Comparator<Residuo> {
         return Double.compare(r1.getPeso(), r2.getPeso());
     }
 }
-
-src\ecotrack\ComparadorPrioridad.java
-package ecotrack;
-
-import java.util.Comparator;
-
-public class ComparadorPrioridad implements Comparator<Residuo> {
-    @Override
-    public int compare(Residuo r1, Residuo r2) {
-        // menor número = mayor prioridad
-        return Integer.compare(r1.getPrioridad(), r2.getPrioridad());
-    }
-}
